7623b3b31be3c6c9fadeaf2b3e7cfcdae18cdb03
Mike Young <mikeyoung85@gmail.com>
2021-03-15 12:52:40 -0400

    try and handle errors from axios better and use nvmrc file to set nodejs version

--- functions/index.js
+++ functions/index.js
@@ -43,7 +43,23 @@ exports.civWebhook = functions.https.onRequest((request, response) => {
       }).then( () => {
         return response.send(message);
       }).catch( () => {
-        return response.status(500).send("It broke!");
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+        return response.send("error")
       })
       return response.send("ok")
     } else {
